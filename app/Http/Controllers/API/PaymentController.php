9898169beee7ea328e0d832bd1e97dfa50b95c74
kennytech <kennyemma2008@gmail.com>
2023-01-14 22:07:12 +0100

    Fixed payment bug

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -43,40 +43,39 @@ class PaymentController extends BaseController
 
             $payment = Payment::create($data);
 
-            if($payment != null) {
-                if($request->payment_title == 'Airtime purchase') 
-                {
-                    $buy_airtime = $this->buyAirtime($request->phone_number, $request->amount);
-    
-                    if($buy_airtime->status == 'success') {
-                       
-                        AirtimePurchase::create([
-                            'user_id' => $request->user_id,
-                            'phone_number' => $request->phone_number,
-                            'flw_ref' => $buy_airtime->data->flw_ref,
-                            'reference' => $buy_airtime->data->reference,
-                            'amount' => $buy_airtime->data->amount,
-                            'network' => $buy_airtime->data->network,
-                            'status' => $buy_airtime->status,
-                            'tx_ref' => $request->tx_ref,
-                            'payment_id' => $payment->id
-                        ]);
-                        
-                    }
-                } 
-                // else 
-                // {
-                //     BillPurchase::create([
-                //         'user_id' => $request->user_id,
-                //         'trans_ref' => $request->trans_ref,
-                //         'amount' => $request->amount,
-                //         'smart_card_number' => $request->smart_card_number,
-                //         'provider' => $request->provider,
-                //         'status' => $request->status,
-                //     ]);
-                // }
-                DB::commit();
-            }
+            if($request->payment_title == 'Buy Airtime') 
+            {
+                $buy_airtime = $this->buyAirtime($request->phone_number, $request->amount);
+
+                if($buy_airtime->status == 'success') {
+                    
+                    AirtimePurchase::create([
+                        'user_id' => $request->user_id,
+                        'phone_number' => $request->phone_number,
+                        'flw_ref' => $buy_airtime->data->flw_ref,
+                        'reference' => $buy_airtime->data->reference,
+                        'amount' => $buy_airtime->data->amount,
+                        'network' => $buy_airtime->data->network,
+                        'status' => $buy_airtime->status,
+                        'tx_ref' => $request->tx_ref,
+                        'payment_id' => $payment->id
+                    ]);
+                    
+                }
+            } 
+            // else 
+            // {
+            //     BillPurchase::create([
+            //         'user_id' => $request->user_id,
+            //         'trans_ref' => $request->trans_ref,
+            //         'amount' => $request->amount,
+            //         'smart_card_number' => $request->smart_card_number,
+            //         'provider' => $request->provider,
+            //         'status' => $request->status,
+            //     ]);
+            // }
+            DB::commit();
+            
             if($payment != null) {
                 return $this->sendResponse($payment, 'Payment successfully created.');
             } else {
