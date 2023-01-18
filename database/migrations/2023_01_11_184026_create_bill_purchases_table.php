53d63a2ade8195eacb7420904e097c14d5e61f80
kennytech <kennyemma2008@gmail.com>
2023-01-18 17:05:07 +0100

    Updated bill purchases table columns

--- database/migrations/2023_01_11_184026_create_bill_purchases_table.php
+++ database/migrations/2023_01_11_184026_create_bill_purchases_table.php
@@ -16,12 +16,16 @@ return new class extends Migration
         Schema::create('bill_purchases', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('trans_ref');
-            $table->integer('amount');
             $table->string('smart_card_number');
-            $table->string('provider');
-            $table->enum('status', ['Successful', 'Failed']);
+            $table->string('flw_ref')->nullable();
+            $table->string('reference')->nullable();
+            $table->decimal('amount');
+            $table->string('network');
+            $table->string('status');
+            $table->string('tx_ref');
+            $table->unsignedBigInteger('payment_id');
             $table->foreign('user_id')->references('id')->on('users');  
+            $table->foreign('payment_id')->references('id')->on('payments');  
             $table->timestamps();
         });
     }
