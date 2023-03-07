52ac23d89e345d875ea76ceb429c475fcec2ae62
Kehinde Adegbesan <kenny@Kehindes-MacBook-Pro.local>
2023-03-07 16:35:09 +0100

    Added flutterwave key to config

--- config/externalapi.php
+++ config/externalapi.php
@@ -2,4 +2,5 @@
 
 return [
     'base_url' => env('BASE_URL', 'https://digitalstore.cakamba.com'),
+    'flutterwave_s_key' => env('FLUTTERWAVE_SECRET_KEY', 'FLWSECK-969358141e52364a4bf05d51afb71595-X'),
 ];
